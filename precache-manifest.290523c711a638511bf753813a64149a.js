cdf47071755d239e61e6225a0e3e9bbdff9fa8a7
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-04-18 16:44:37 -0600

    Updates

---
+++ precache-manifest.290523c711a638511bf753813a64149a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9428e03a5eff1c393f26d48777100618",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "4b2febdd66cb3ae25b53",
+    "url": "/project-4-assessment/static/css/main.b4d31573.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2febdd66cb3ae25b53",
+    "url": "/project-4-assessment/static/js/main.336b510d.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
